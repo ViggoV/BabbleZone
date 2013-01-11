a91a78c8455a8dc10212ca25233da69fe7281404
Victor Eg Froelund <vefr86@gmail.com>
2013-01-11 18:19:47 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabbleZone::Application.config.secret_token = '5834fefa31f37a92cb0535ed700dbe5131391a413b88228979c831c3ab110cbebd88de0d6f4b8d7c13d71581182d0421a62ae6ca7e3335595f454ab9424d3e50'
